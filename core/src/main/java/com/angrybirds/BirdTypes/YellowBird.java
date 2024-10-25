2ddd6e0b17d0039e675a03c0c6e88ef264e86aff
cipher-aman <162812360+cipher-aman@users.noreply.github.com>
2024-10-25 21:02:54 +0530

    Update YellowBird.java

--- core/src/main/java/com/angrybirds/BirdTypes/YellowBird.java
+++ core/src/main/java/com/angrybirds/BirdTypes/YellowBird.java
@@ -1,1 +1,25 @@
+package com.angrybirds.BirdTypes;
 
+import com.angrybirds.PigTypes.Pigs;
+import com.angrybirds.StructureTypes.Structures;
+import com.badlogic.gdx.graphics.Texture;
+
+public class YellowBird extends Birds {
+    public YellowBird(Texture texture) {
+        super(texture); // Use the default constructor
+    }
+
+    public YellowBird(Texture texture, float x, float y) {
+        super(texture, x, y); // Set position during creation
+    }
+
+    @Override
+    public void hitStructure(Structures structure) {
+        structure.takeDamage(0.5f); // Less impact for the YellowBird
+    }
+
+    @Override
+    public void hitPig(Pigs pig) {
+        pig.takeDamage(0.5f); // Less impact for the YellowBird
+    }
+}
