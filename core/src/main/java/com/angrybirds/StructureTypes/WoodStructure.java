8d66e369df9c6c394279a0ed4b0448e33c81fdee
Aryaman Singh <162812886+aryaman-2307@users.noreply.github.com>
2024-10-25 17:21:03 +0530

    Update WoodStructure.java

--- core/src/main/java/com/angrybirds/StructureTypes/WoodStructure.java
+++ core/src/main/java/com/angrybirds/StructureTypes/WoodStructure.java
@@ -1,9 +1,1 @@
-package com.angrybirds.StructureTypes;
 
-import com.badlogic.gdx.graphics.Texture;
-
-public class WoodStructure extends Structures {
-    public WoodStructure(Texture texture, float x, float y) {
-        super(texture, 2, x, y); // 2 hits to destroy
-    }
-}
