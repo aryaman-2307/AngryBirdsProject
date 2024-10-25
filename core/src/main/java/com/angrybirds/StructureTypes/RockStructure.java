366e18fbb772d88d3bed43467a17da7a522ff3f3
Aryaman Singh <162812886+aryaman-2307@users.noreply.github.com>
2024-10-25 17:20:37 +0530

    Update RockStructure.java

--- core/src/main/java/com/angrybirds/StructureTypes/RockStructure.java
+++ core/src/main/java/com/angrybirds/StructureTypes/RockStructure.java
@@ -1,9 +1,1 @@
-package com.angrybirds.StructureTypes;
 
-import com.badlogic.gdx.graphics.Texture;
-
-public class RockStructure extends Structures {
-    public RockStructure(Texture texture, float x, float y) {
-        super(texture, 4, x, y); // 4 hits to destroy
-    }
-}
